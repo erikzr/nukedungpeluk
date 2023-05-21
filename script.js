9987cec015781270ff4e56b0e2e2ae3ad7416354
erikzr <muhammaderikzubairrohman@gmail.com>
2023-05-21 23:06:20 +0700

    menambah photocard

---
+++ script.js
@@ -0,0 +1,11 @@
+var links = document.getElementsByTagName("a");
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("mouseup", function() {
+    this.blur();
+  });
+}
+
+var noCopyElement = document.getElementById('click');
+  noCopyElement.addEventListener('copy', function(event) {
+    event.preventDefault();
+  });
